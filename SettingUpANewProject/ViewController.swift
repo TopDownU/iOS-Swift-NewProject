3f3917b454e16334b86801fcffbdbd9c1eaca839
vdthatte <vd_thatte@yahoo.com>
2017-11-25 16:10:31 -0500

    initial commit

---
+++ SettingUpANewProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SettingUpANewProject
+//
+//  Created by Vidyadhar V. Thatte on 11/25/17.
+//  Copyright © 2017 VdThatte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
